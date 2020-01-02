f8a9264927989783ad38b91139d9bb4ed963a591
NumbSkullq1 <wolfstreetmack@gmail.com>
2020-01-01 20:21:47 -1200

    Updates

---
+++ precache-manifest.498f25947461f7d9436ed5a7872b9226.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c69776adcca4269e0054dcf9d12821",
+    "url": "/NSQ1-portfolio/index.html"
+  },
+  {
+    "revision": "9308c9b1c758ec7b6b8a",
+    "url": "/NSQ1-portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a4e69f717df44694b563",
+    "url": "/NSQ1-portfolio/static/js/2.3881883a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/NSQ1-portfolio/static/js/2.3881883a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9308c9b1c758ec7b6b8a",
+    "url": "/NSQ1-portfolio/static/js/main.d26023d4.chunk.js"
+  },
+  {
+    "revision": "79f23afb07a317d0aaad",
+    "url": "/NSQ1-portfolio/static/js/runtime-main.55304e3b.js"
+  }
+]);
